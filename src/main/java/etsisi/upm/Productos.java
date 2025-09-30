0f05c8c82463f3eb00ed66b002584b19442ebfb2
arang06 <aranchanunez.g@gmail.com>
2025-09-30 17:49:13 +0200

    Update Productos.java
    He creado los getters y los setters para los atributos de la clase prductos segun el UML

--- src/main/java/etsisi/upm/Productos.java
+++ src/main/java/etsisi/upm/Productos.java
@@ -1,4 +1,38 @@
 package etsisi.upm;
 
+
 public class Productos {
+  ///Asignamos las variables
+  private int id;
+  private String name;
+  private float price;
+  private String nameOfCategory;
+
+  //getters y setters
+  public void setId ( int id){
+  this.id = id;
+  }
+  public int getId (){
+    return id;
+  }
+  public void setName(String name){
+    this.name = name; 
+  }
+  public String getName(){
+    return name;
+  }
+  public void setPrice(float price){
+    this.price = price;
+  }
+  public float getPrice(){
+    return price;
+  }
+  public void setnameOfCategory( String category){
+    this.nameOfCategory = category;
+  }
+   public String getnameOfCategory(){
+    return nameOfCategory ;
+  }
+  
+  
 }
