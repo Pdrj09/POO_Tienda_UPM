1d80988bd995edf5d50e133aaac0b2093e182a2b
Qq Ww <andreaesteparobles@gmail.com>
2025-10-07 14:16:29 +0200

    se han arreglado cositas del enum Categories

--- src/main/java/etsisi/upm/models/Categories.java
+++ src/main/java/etsisi/upm/models/Categories.java
@@ -3,11 +3,20 @@ package etsisi.upm.models;
 /// ESTO HAY QUE USARLO EN MENU, HAY QUE CHECKEAR SI EXISTE LA CATEGORIA O NO ANTES
 /// DE CREAR UN PRODUCTO
 public enum Categories {
-    MERCH,
-    STATIONERY,
-    CLOTHING,
-    BOOK,
-    ELECTRONICS;
+    MERCH(0.0),
+    STATIONERY(0.05),
+    CLOTHING(0.07),
+    BOOK(0.1),
+    ELECTRONICS(0.03);
+    private final double discount;
+
+    Categories(double discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return this.discount;
+    }
 
     //verifies is a category exists
     public static boolean existCategory(String category) {
