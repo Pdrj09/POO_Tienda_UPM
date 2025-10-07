d9aadd87d01c5cd2bc1ff9f4a29a5933d2eaa12d
Pegaso <pegasojuegos@gmail.com>
2025-10-07 12:31:30 +0200

    Ticket list to hasMap in progres

--- src/main/java/etsisi/upm/Ticket.java
+++ src/main/java/etsisi/upm/Ticket.java
@@ -1,43 +1,45 @@
 package etsisi.upm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class Ticket {
-    public List<Product> list = new ArrayList<>();
+    public HashMap<Product,Integer> list = new HashMap<>();
 
-    /// The ticket goes empty despite the products it has.
+    // The ticket goes empty despite the products it has.
     public void clear (){
         list.clear();
     }
 
-    /// Add a created product to the Ticket with the amount. It goes to the end of the list.
+    // Add a product to the ticket, if the product already exists increments its amount
     public void add(Product p, int amount){
-        // First we have to search if there's an instance of the product (id) in the ticket.
-        for(Product prod : list){       // (loop through each element of the current list...)
-            if(prod.getId() == p.getId()){
-                prod.setAmount(prod.getAmount() + amount);
-                return;
+        this.list.put(p,this.list.containsKey(p) ? this.list.get(p)+amount : amount );
+    }
+
+    // Remove a product from the ticket
+    public void remove(Product p){
+        this.list.remove(p);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Iterator<HashMap<Product,Integer>> ite = new Iterator<HashMap<Product, Integer>>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public HashMap<Product, Integer> next() {
+                return null;
             }
         }
-
-        //if product is not found
-        p.setAmount(amount);
-        list.add(p);
+        return res.toString();
     }
 
-    public void remove(Product p){
-        for(Product prod : list){       // (loop through each element of the current list...)
-            if(prod.getId() == p.getId()){
-                list.remove(p);
-                return;
-            }
-        } //There will always be only one instance of a product.
-    }
-
-    public void print(){
-        for ()
-    }
     // ticket print (imprime factura)
     ///  Decirle a Pedro que para el add en ticket tiene que imprimir el producto y su descripcion = al amount.
 }
