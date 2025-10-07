98495036503fb4cd083920dba51ef483382b04a4
Qq Ww <andreaesteparobles@gmail.com>
2025-10-07 12:12:30 +0200

    comandos de ticket y product puestos, main corregido

--- src/main/java/etsisi/upm/Menu.java
+++ src/main/java/etsisi/upm/Menu.java
@@ -22,6 +22,9 @@ public class Menu {
             echo "<texto>"
             help
             exit
+            Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS
+                Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%,
+                ELECTRONICS 3%.
         """;
 
     private static final String BYE = """
@@ -35,6 +38,16 @@ public class Menu {
     private static final String ECHO =  "echo";
     private static final String HELP = "help";
 
+    private static final String TICKET_ADD =  "add";
+    private static final String TICKET_PRINT =  "print";
+    private static final String TICKET_NEW = "new";
+    private static final String TICKET_REMOVE = "remove";
+
+
+    private static final String PRODUCT_ADD =  "add";
+    private static final String PRODUCT_LIST =  "list";
+    private static final String PRODUCT_UPDATE = "update";
+    private static final String PRODUCT_REMOVE = "remove";
 
     public void menu() {
         System.out.println(WELCOME_MESSAGE);
@@ -44,7 +57,7 @@ public class Menu {
         if (query.startsWith(PROD)) {
 
         } else if (query.startsWith(TICKET)) {
-
+            this.ticketQuery(query);
         } else if (query.startsWith(ECHO)) {
             this.echoCommand(query);
         } else if (query.startsWith(HELP)) {
@@ -59,10 +72,27 @@ public class Menu {
     }
 
     private void prodQuery(String query) {
+        if (query.contains(PRODUCT_ADD)){
 
+        }else if (query.contains(PRODUCT_LIST)){
+
+        }else if (query.contains(PRODUCT_REMOVE)){
+
+        }else if(query.contains(PRODUCT_UPDATE)){
+
+        }
     }
 
     private void ticketQuery(String query) {
+        if (query.contains(TICKET_ADD)){
+
+        }else if (query.contains(TICKET_NEW)){
+
+        }else if (query.contains(TICKET_PRINT)){
+
+        }else if(query.contains(TICKET_REMOVE)){
+
+        }
     }
 
     private void help() {
