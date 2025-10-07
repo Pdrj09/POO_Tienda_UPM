b187c614623fb72131b8ff2869f38181c53a23bb
Pegaso <pegasojuegos@gmail.com>
2025-10-07 14:17:14 +0200

    in progress...

--- src/main/java/etsisi/upm/models/Ticket.java
+++ src/main/java/etsisi/upm/models/Ticket.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class Ticket {
-    public HashMap<Product,Integer> list = new HashMap<>();
+    private HashMap<Product,Integer> list = new HashMap<>();
+    private HashMap<String,Integer> categories = new HashMap<>(); //Cambiar por categorias UwU
 
     // The ticket goes empty despite the products it has.
     public void clear (){
@@ -14,6 +15,8 @@ public class Ticket {
     // Add a product to the ticket, if the product already exists increments its amount
     public void add(Product p, int amount){
         this.list.put(p,this.list.containsKey(p) ? this.list.get(p)+amount : amount );
+        Categories category = p.getCategory();
+        this.categories.put(category,this.categories.containsKey(category) ? this.categories.get(category)+1:1);
     }
 
     // Remove a product from the ticket
@@ -21,20 +24,23 @@ public class Ticket {
         this.list.remove(p);
     }
 
+    private double totaPrice(){
+        return list.entrySet()
+                .stream()
+                .mapToDouble(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
+    }
+
+    private double totalDiscount(){
+        return list.entrySet()
+                .stream()
+                .mapToDouble()
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        Iterator<HashMap<Product,Integer>> ite = new Iterator<HashMap<Product, Integer>>() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
 
-            @Override
-            public HashMap<Product, Integer> next() {
-                return null;
-            }
-        }
         return res.toString();
     }
 
