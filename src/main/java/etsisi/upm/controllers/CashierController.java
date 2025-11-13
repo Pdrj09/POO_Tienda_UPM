6397dfd2c347e40680b4a61c0f3bd57f38e28284
Pdrj09 <pjl.dnunez@alummnos.upm.es>
2025-11-13 23:24:33 +0100

    feat: Cashier add, remove and list methods on controller

--- src/main/java/etsisi/upm/controllers/CashierController.java
+++ src/main/java/etsisi/upm/controllers/CashierController.java
@@ -1,26 +1,54 @@
 package etsisi.upm.controllers;
 
-import etsisi.upm.models.repositories.CashRepository;
+import etsisi.upm.models.repositories.CashierRepository;
+import etsisi.upm.models.repositories.Repository;
 import etsisi.upm.models.repositories.RepositoryInterface;
 import etsisi.upm.models.users.Cashier;
 
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.Random;
-import java.util.Set;
 
 public class CashierController {
     private static final Random ramdon = new Random();
-    private final RepositoryInterface<Cashier> repository;
+    private final Repository<String, Cashier> repository;
 
     private static final String CASHIER_PREFIX = "UW";
     private static final String CASH_REGEX = "%s%07d";
 
+    private static final String DUPLICATED_ID_ERROR  = "El id pasado como pararametro ya existe, añada otro";
+
     public CashierController() {
-        repository = new CashRepository();
+        repository = new Repository<>();
     }
 
-    public void addCash(String emailCompany, String name) {
+    public Cashier addCash(String emailCompany, String name) {
+        //TODO change cashier
+        //TODO comprobar que el id sea correcto
+        String cashierId = generateCashierId();
+        Cashier cashier =  new Cashier(emailCompany, name);
 
+        repository.add(cashierId, cashier);
+
+        return cashier;
+    }
+
+    public Cashier addCashier(String cahierId, String emailCompany, String name) {
+        if(repository.findById(cahierId) == null) {
+            Cashier cashier =  new Cashier(cahierId, emailCompany, name);
+            repository.add(cahierId, cashier);
+
+            return cashier;
+        } else {
+            throw new IllegalArgumentException(DUPLICATED_ID_ERROR);
+        }
+    }
+
+    public Cashier removeCashier(String id) {
+        return repository.removeById(id);
+    }
+
+    public Collection<Cashier> listCashiers() {
+        return repository.findAll();
     }
 
     private String generateCashierId(){
