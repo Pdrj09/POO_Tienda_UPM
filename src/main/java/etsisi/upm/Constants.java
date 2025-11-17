be20158ffea4d532455ae276673cc79f0e79ed5d
Arancha <aranchanunez.g@gmail.com>
2025-11-17 21:35:10 +0100

    Constantes globales

---
+++ src/main/java/etsisi/upm/Constants.java
@@ -0,0 +1,65 @@
+package etsisi.upm;
+
+public class Constants {
+
+
+    public static final int QUERY_SUCCESS = 1;
+    public  static final int QUERY_EXIT = 0;
+
+    // numbers
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+
+    // menu const
+    public static final String EXIT = "exit";
+    public static final String PROD = "prod";
+    public static final String TICKET = "ticket";
+    public static final String ECHO = "echo";
+    public static final String HELP = "help";
+    public static final String CLIENT = "client";
+    public static final String CASH = "cash";
+
+    // str const
+    public static final String STR_EMPTY = "";
+    public static final String STR_DOT = ".";
+    public static final String STR_COMMA = ",";
+    public static final String STR_ERROR = "Error";
+    public static final String STR_BLANK_SPACE = " ";
+    public static final String STR_DOUBLE_DOT = ":";
+
+
+
+    public static final String OK_STATUS = "ok";  //Ok
+    public static final String ERROR_STATUS = "Error"; //error
+
+
+    // regex const
+    public static final String REGEX_INIT = "^";
+    public static final String REGEX_BLANK_SPACE = "\\s*";
+    public static final String REGEX_DOUBLE_QUOTE = "\"";
+    public static final String REGEX_TO_SPLIT = " (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+    public static final String REGEX_PERSONALIZED = "(?<=--p)";
+
+
+    // products const
+    public static final String PRODUCT_ADD = "add";
+    public static final String PRODUCT_LIST = "list";
+    public static final String PRODUCT_UPDATE = "update";
+    public static final String PRODUCT_REMOVE = "remove";
+
+    // ticket const
+    public static final String TICKET_ADD = "add";
+    public static final String TICKET_PRINT = "print";
+    public static final String TICKET_NEW = "new";
+    public static final String TICKET_REMOVE = "remove";
+
+    // client const
+    public static final String CLIENT_ADD = "add";
+    public static final String CLIENT_REMOVE = "remove";
+    public static final String CLIENT_LIST = "list";
+
+}
--- src/main/java/etsisi/upm/io/CLI.java
+++ src/main/java/etsisi/upm/io/CLI.java
@@ -2,6 +2,7 @@ package etsisi.upm.io;
 
 import etsisi.upm.controllers.ProductController;
 import etsisi.upm.controllers.TicketController;
+import etsisi.upm.Constants;
 
 public class CLI {
 
@@ -9,64 +10,6 @@ public class CLI {
     private final ProductController productController;  //gobal variable called controller
 
     // status code
-    private static final int QUERY_SUCCESS = 1;
-    private static final int QUERY_EXIT = 0;
-
-    // numbers
-    private static final int ONE = 1;
-    private static final int TWO = 2;
-    private static final int THREE = 3;
-    private static final int FOUR = 4;
-    private static final int FIVE = 5;
-    private static final int SIX = 6;
-
-    // menu const
-    private static final String EXIT = "exit";
-    private static final String PROD = "prod";
-    private static final String TICKET = "ticket";
-    private static final String ECHO = "echo";
-    private static final String HELP = "help";
-    private static final String CLIENT = "client";
-    private static final String CASH = "cash";
-
-    // str const
-    private static final String STR_EMPTY = "";
-    private static final String STR_DOT = ".";
-    private static final String STR_COMMA = ",";
-    private static final String STR_ERROR = "Error";
-    private static final String STR_BLANK_SPACE = " ";
-    private static final String STR_DOUBLE_DOT = ":";
-
-
-
-    private static final String OK_STATUS = "ok";  //Ok
-    private static final String ERROR_STATUS = "Error"; //error
-
-
-    // regex const
-    private static final String REGEX_INIT = "^";
-    private static final String REGEX_BLANK_SPACE = "\\s*";
-    private static final String REGEX_DOUBLE_QUOTE = "\"";
-    private static final String REGEX_TO_SPLIT = " (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-    private static final String REGEX_PERSONALIZED = "(?<=--p)";
-
-
-    // products const
-    private static final String PRODUCT_ADD = "add";
-    private static final String PRODUCT_LIST = "list";
-    private static final String PRODUCT_UPDATE = "update";
-    private static final String PRODUCT_REMOVE = "remove";
-
-    // ticket const
-    private static final String TICKET_ADD = "add";
-    private static final String TICKET_PRINT = "print";
-    private static final String TICKET_NEW = "new";
-    private static final String TICKET_REMOVE = "remove";
-
-    // client const
-    private static final String CLIENT_ADD = "add";
-    private static final String CLIENT_REMOVE = "remove";
-    private static final String CLIENT_LIST = "list";
 
     public CLI() {
         ViewCLI.printWellcomeMessage();
@@ -75,29 +18,29 @@ public class CLI {
 
     public int newQuery(String query) {
         //Check if the query starts with PROD command keyword
-        if (query.startsWith(PROD)) {
-            this.prodQuery(deleteSubstring(query, createGeneralRegex(PROD)));
+        if (query.startsWith(Constants.PROD)) {
+            this.prodQuery(deleteSubstring(query, createGeneralRegex(Constants.PROD)));
             //if the query starts with TICKET, we handle using ticketQuery().
-        } else if (query.startsWith(TICKET)) {
-            this.ticketQuery(deleteSubstring(query, createGeneralRegex(TICKET)));
+        } else if (query.startsWith(Constants.TICKET)) {
+            this.ticketQuery(deleteSubstring(query, createGeneralRegex(Constants.TICKET)));
             //if query starts with ECHO,it echoes back the input
-        } else if (query.startsWith(ECHO)) {
+        } else if (query.startsWith(Constants.ECHO)) {
             ViewCLI.echoCommand(query);
             //if query starts with HELP, displays help information available
-        } else if (query.startsWith(HELP)) {
+        } else if (query.startsWith(Constants.HELP)) {
             ViewCLI.printHelp();
             //if query starts with EXIT prints goodbye message
-        } else if (query.startsWith(EXIT)) {
+        } else if (query.startsWith(Constants.EXIT)) {
             ViewCLI.printExit();
-            return QUERY_EXIT;
+            return Constants.QUERY_EXIT;
             //returns 0
-        }else if (query.startsWith(CLIENT)){
+        }else if (query.startsWith(Constants.CLIENT)){
             this.clientQuery(query);
-        }else if (query.startsWith(CASH)){
+        }else if (query.startsWith(Constants.CASH)){
             //this.cashQuery(query);
         }
 
-        return QUERY_SUCCESS;
+        return Constants.QUERY_SUCCESS;
         //returns 1
     }
     //client add "<nombre>" <DNI> <email> <cashId>
@@ -105,50 +48,50 @@ public class CLI {
     //client list
 
     private void clientQuery(String query){
-        String[] querySplit = query.split(REGEX_TO_SPLIT);
-        if(query.contains(CLIENT_ADD)){
+        String[] querySplit = query.split(Constants.REGEX_TO_SPLIT);
+        if(query.contains(Constants.CLIENT_ADD)){
             
-        }else if (query.contains(CLIENT_REMOVE)){
+        }else if (query.contains(Constants.CLIENT_REMOVE)){
 
-        }else if (query.contains(CLIENT_LIST)){
+        }else if (query.contains(Constants.CLIENT_LIST)){
 
 
         }
     }
 
     private void prodQuery(String query) {
-        String[] querySplit = query.split(REGEX_TO_SPLIT);
+        String[] querySplit = query.split(Constants.REGEX_TO_SPLIT);
         try {
-            if (query.contains(PRODUCT_ADD)) {
+            if (query.contains(Constants.PRODUCT_ADD)) {
 
                  ViewCLI.print(ProductController.productAdder(querySplit , productController));
 
-            } else if (query.contains(PRODUCT_LIST)) {
+            } else if (query.contains(Constants.PRODUCT_LIST)) {
 
                 System.out.println(productController.prodList());
-                okStatus(PROD, PRODUCT_LIST);
+                okStatus(Constants.PROD, Constants.PRODUCT_LIST);
 
-            } else if (query.contains(PRODUCT_REMOVE)) {
+            } else if (query.contains(Constants.PRODUCT_REMOVE)) {
                 productController.prodDelete(productController, query);
 
-            } else if (query.contains(PRODUCT_UPDATE)) {
+            } else if (query.contains(Constants.PRODUCT_UPDATE)) {
 
 
-                int id = Integer.parseInt(querySplit[ONE]);
+                int id = Integer.parseInt(querySplit[Constants.ONE]);
 
 
-                String productEdited = productController.updateProduct(id, querySplit[TWO], querySplit[THREE]);
+                String productEdited = productController.updateProduct(id, querySplit[Constants.TWO], querySplit[Constants.THREE]);
 
                 if (productEdited != null) {
                     System.out.println(productEdited);
-                    okStatus(TICKET, TICKET_NEW);
+                    okStatus(Constants.TICKET, Constants.TICKET_NEW);
                 } else {
-                    errorStatus(TICKET, TICKET_NEW);
+                    errorStatus(Constants.TICKET, Constants.TICKET_NEW);
                 }
 
             }
         } catch (Exception e) {
-            errorStatus(PROD, PRODUCT_ADD, e.toString());
+            errorStatus(Constants.PROD, Constants.PRODUCT_ADD, e.toString());
         }
     }
     /*ticket new [<id>] <cashId> <userId>
@@ -158,48 +101,48 @@ public class CLI {
     ticket list*/
 
     private void ticketQuery(String query) {
-        String[] querySplit = query.split(REGEX_TO_SPLIT);
-        String ticketId = querySplit[ONE];
+        String[] querySplit = query.split(Constants.REGEX_TO_SPLIT);
+        String ticketId = querySplit[Constants.ONE];
 
-        if (query.contains(TICKET_ADD)) {
+        if (query.contains(Constants.TICKET_ADD)) {
 
             //ticket add <ticketId><cashId> <prodId> <amount> [--p<txt> --p<txt>]
 
 
-            String cashId = querySplit[TWO];
-            int id = Integer.parseInt(querySplit[THREE]);
+            String cashId = querySplit[Constants.TWO];
+            int id = Integer.parseInt(querySplit[Constants.THREE]);
 
-            int quantity = Integer.parseInt(querySplit[FOUR]);
+            int quantity = Integer.parseInt(querySplit[Constants.FOUR]);
 
             String newTicket = "";
             //if it is a personalized prod
 
             if (querySplit [querySplit.length-1].contains("--p") ){
-                String[] queryPersonalized = querySplit[FIVE].split(REGEX_TO_SPLIT);
+                String[] queryPersonalized = querySplit[Constants.FIVE].split(Constants.REGEX_TO_SPLIT);
                 /// newTicket = controller.addPersonalicedProductToTicket(ticketId, cashId, id, quantity, queryPersonalized);
             }else {
                 /// newTicket =  controller.addProductToTicket(ticketId, cashId, id, quantity);
             }
             if (newTicket != null) {
                 System.out.println(newTicket);
-                okStatus(TICKET, TICKET_ADD);
+                okStatus(Constants.TICKET, Constants.TICKET_ADD);
             } else {
-                errorStatus(TICKET, TICKET_ADD);
+                errorStatus(Constants.TICKET,Constants. TICKET_ADD);
             }
 
-        } else if (query.contains(TICKET_NEW)) {
+        } else if (query.contains(Constants.TICKET_NEW)) {
 
             productController.ticketNew();
-            okStatus(TICKET, TICKET_NEW);
+            okStatus(Constants.TICKET,Constants.TICKET_NEW);
 
 
-        } else if (query.contains(TICKET_PRINT)) {
+        } else if (query.contains(Constants.TICKET_PRINT)) {
 
             System.out.println(productController.ticketPrint());
-            okStatus(TICKET, TICKET_PRINT);
+            okStatus(Constants.TICKET, Constants.TICKET_PRINT);
 
-        } else if (query.contains(TICKET_REMOVE)) {
-            int id = Integer.parseInt(deleteSubstring(query, createGeneralRegex(PRODUCT_REMOVE)));
+        } else if (query.contains(Constants.TICKET_REMOVE)) {
+            int id = Integer.parseInt(deleteSubstring(query, createGeneralRegex(Constants.PRODUCT_REMOVE)));
 
            // if (controller.removeProductFromTicket(ticketId,id)) {
            //     System.out.println(okStatus(TICKET, TICKET_REMOVE));
@@ -213,9 +156,9 @@ public class CLI {
         StringBuilder stringBuilder;
         stringBuilder = new StringBuilder();
 
-        stringBuilder.append(REGEX_INIT)
+        stringBuilder.append(Constants.REGEX_INIT)
                 .append(query)
-                .append(REGEX_BLANK_SPACE);
+                .append(Constants.REGEX_BLANK_SPACE);
 
         return stringBuilder.toString();
     }
@@ -227,11 +170,11 @@ public class CLI {
         builder = new StringBuilder();
 
         builder.append(type)
-                .append(STR_BLANK_SPACE)
+                .append(Constants.STR_BLANK_SPACE)
                 .append(comand)
-                .append(STR_DOUBLE_DOT)
-                .append(STR_BLANK_SPACE)
-                .append(OK_STATUS);
+                .append(Constants.STR_DOUBLE_DOT)
+                .append(Constants.STR_BLANK_SPACE)
+                .append(Constants.OK_STATUS);
 
         ViewCLI.print(builder.toString());
     }
@@ -241,13 +184,13 @@ public class CLI {
         builder = new StringBuilder();
 
         builder.append(type)
-                .append(STR_BLANK_SPACE)
+                .append(Constants.STR_BLANK_SPACE)
                 .append(comand)
-                .append(STR_DOUBLE_DOT)
-                .append(STR_BLANK_SPACE)
-                .append(ERROR_STATUS)
-                .append(STR_DOUBLE_DOT)
-                .append(STR_BLANK_SPACE)
+                .append(Constants.STR_DOUBLE_DOT)
+                .append(Constants.STR_BLANK_SPACE)
+                .append(Constants.ERROR_STATUS)
+                .append(Constants.STR_DOUBLE_DOT)
+                .append(Constants.STR_BLANK_SPACE)
                 .append(message);
 
         ViewCLI.print(builder.toString());
@@ -257,18 +200,18 @@ public class CLI {
         StringBuilder builder = new StringBuilder();
 
         builder.append(type)
-                .append(STR_BLANK_SPACE)
+                .append(Constants.STR_BLANK_SPACE)
                 .append(comand)
-                .append(STR_DOUBLE_DOT)
-                .append(STR_BLANK_SPACE)
-                .append(ERROR_STATUS);
+                .append(Constants.STR_DOUBLE_DOT)
+                .append(Constants.STR_BLANK_SPACE)
+                .append(Constants.ERROR_STATUS);
 
         ViewCLI.print(builder.toString());
     }
 
 
     private static String deleteSubstring(String query, String regex) {
-        return query.replaceFirst(regex, STR_EMPTY);
+        return query.replaceFirst(regex, Constants.STR_EMPTY);
     }
 
 
