0a4e27ff266929bf21903074b8afbd8a3d590114
Qq Ww <andreaesteparobles@gmail.com>
2025-11-27 18:13:28 +0100

    si veo más codigo que se solapo, lo subo

--- src/main/java/etsisi/upm/io/View.java
+++ src/main/java/etsisi/upm/io/View.java
@@ -5,6 +5,8 @@ import etsisi.upm.models.Ticket;
 import etsisi.upm.models.users.Cashier;
 import etsisi.upm.models.users.Client;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class View {
@@ -37,32 +39,88 @@ public class View {
             }
             //for knowing the first type of the element
             Class<?> itemType = collection.iterator().next().getClass();
-            for (Object item : collection) {
-                sb.append(typeHeader(itemType));
-                sb.append(item).append("\n");
-            }
+            sb.append(buildTable(collection, itemType));
             return sb;
         }
 
-        //for arrays
-        if (element.getClass().isArray()) {
-            int length = java.lang.reflect.Array.getLength(element);
-            if (length == 0) {
-                sb.append(emptyMessage(null)).append("\n");
-                return sb;
-            }
-            for (int i = 0; i < length; i++) {
-                Object item = java.lang.reflect.Array.get(element, i);
-                sb.append(typeHeader(item.getClass()));
-                sb.append(item).append("\n");
-            }
-            return sb;
-        }
-
-        //normal object
-        sb.append(typeHeader(element.getClass()));
-        sb.append(element).append("\n");
+        sb.append(buildTable(java.util.List.of(element), element.getClass()));
         return sb;
+    }
+
+    private static String buildTable(Collection<?> collection, Class<?> itemType) {
+        StringBuilder sb = new StringBuilder();
+
+        Field[] fields = itemType.getDeclaredFields();
+        for (Field f :fields) f.setAccessible(true);
+        //we calculate the widths
+        int[] colWidths = new int[fields.length];
+        for (int i = 0; i <fields.length; i++)
+            colWidths[i] = fields[i].getName().length();
+        for (Object obj :collection) {
+            for (int i = 0; i < fields.length;i++) {
+                try {
+                    Object value = fields[i].get(obj);
+                    int len = value!=null?value.toString().length(): 4; //for null
+                    if (len >colWidths[i]) colWidths[i] = len;
+                } catch (Exception ignored) {}
+            }
+        }
+
+        String title = " " + itemType.getSimpleName().toLowerCase() + " ";
+        int totalWidth = Arrays.stream(colWidths).sum()+fields.length * 3 + 1;
+
+        //header
+        sb.append(YELLOW)
+                .append("┌")
+                .append(centerTitle(totalWidth - 2, title))
+                .append("┐")
+                .append("\n");
+
+        //columns names
+        sb.append("│ ");
+        for (int i = 0; i <fields.length; i++) {
+            sb.append(String.format("%-" + colWidths[i] + "s │ ", fields[i].getName()));
+        }
+        sb.append("\n");
+
+        //separator
+        sb.append("├");
+        for (int width :colWidths) {
+            sb.append("─".repeat(width +2)).append("┼");
+        }
+        sb.setLength(sb.length()- 1); //the last one out
+        sb.append("\n");
+
+        //rows
+        sb.append(CYAN);
+        for (Object obj : collection) {
+            sb.append("│ ");
+            for (int i = 0; i < fields.length; i++) {
+                try {
+                    Object value =fields[i].get(obj);
+                    String val = value != null ? value.toString() : "null";
+                    sb.append(String.format("%-" + colWidths[i] + "s │ ", val));
+                } catch (Exception e) {
+                    sb.append("ERROR │ ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        //footer
+        sb.append(CYAN)
+                .append("└")
+                .append("─".repeat(totalWidth - 2))
+                .append("┘")
+                .append(RESET)
+                .append("\n");
+
+        return sb.toString();
+    }
+
+    private static String centerTitle(int i, String title) {
+        int padding = (i-title.length())/2;
+        return "─".repeat(padding) +title +"─".repeat(i - padding - title.length());
     }
 
 
@@ -71,12 +129,5 @@ public class View {
         if (type == null)
             return MSG_NOTHING_TO_SHOW + "\n";
         return YELLOW + "[!] No " + type.getSimpleName().toLowerCase() + "s found." + RESET;
-    }
-
-    //Prints a header based on the object type.
-    private static String typeHeader(Class<?> type) {
-        if (type == null)
-            return MSG_NOTHING_TO_SHOW + "\n";
-        return CYAN + type.getSimpleName().toLowerCase() + RESET + "\n";
     }
 }
