a6b9403bb573cce4cbdfdec26197da1cdaea97e8
Qq Ww <andreaesteparobles@gmail.com>
2025-11-28 16:06:49 +0100

    arreglado, ya es funcional al completo

--- src/main/java/etsisi/upm/controllers/ClientController.java
+++ src/main/java/etsisi/upm/controllers/ClientController.java
@@ -11,9 +11,6 @@ import java.util.Collection;
 public class ClientController {
     private final Repository<String, Client> clientRepository;
     private final Repository<String, Cashier> cashierRepository;
-    private static final String DUPLICATED_ID_ERROR  = "El id pasado como pararametro ya existe, añada otro";
-    private static final String CASIER_NOT_EXIST  = "El casier dado no existe";
-
 
     public ClientController(Repository<String, Client> repository, Repository<String, Cashier> cashierRepository) {
         this.clientRepository = repository;
@@ -21,66 +18,28 @@ public class ClientController {
     }
 
     public String clientQuery(String[] query) {
-        StringBuilder regex = new StringBuilder();
-        regex.append(Constants.REGEX_INIT);
-
-        if (query[Constants.QUERY_CASH_POS_INSTRUCTION].equals(Constants.CASH_ADD)) {
-            if (query.length == Constants.QUERY_CLIENT_POS_MAXARGS){
-                Client newClient = addClient(query[Constants.TWO], query[Constants.THREE],
-                        query[Constants.FOUR], query[Constants.FIVE]);
-                return View.getString(newClient);
+        switch (query[Constants.QUERY_CASH_POS_INSTRUCTION]) {
+            case Constants.CASH_ADD -> {
+                if (query.length == Constants.QUERY_CLIENT_POS_MAXARGS) {
+                    Client newClient = addClient(query[Constants.TWO], query[Constants.THREE],
+                            query[Constants.FOUR], query[Constants.FIVE]);
+                    return View.getString(newClient);
+                } else
+                    throw new IllegalArgumentException(Constants.ERROR_FEW_PARAMS);
             }
-        }/*
-        else if (query.startsWith(Constants.CLIENT_LIST)) {
-            regex.append(Constants.CLIENT_LIST)
-                    .append(Constants.REGEX_BLANK_SPACE);
-            query = query.replaceFirst(Constants.CLIENT_LIST, Constants.STR_EMPTY);
-
-            Collection<Client> clients = listClients();
-
-            //TODO formatear
-            StringBuilder list = new StringBuilder();
-            for (Client client : clients) {
-                list.append(View.getString(client));
+            case Constants.CLIENT_LIST -> {
+                Collection<Client> clients = listClients();
+                if (clients.isEmpty())
+                    return Constants.ERROR_NO_CLIENTS_FOUND;
+                return View.getString(clients);
             }
-
-            return list.toString();
-
-        } else if (query.startsWith(Constants.CLIENT_REMOVE)) {
-            regex.append(Constants.CLIENT_REMOVE)
-                    .append(Constants.REGEX_BLANK_SPACE);
-            query = query.replaceFirst(Constants.CLIENT_REMOVE, Constants.STR_EMPTY);
-
-            String[] query = query.split(Constants.REGEX_INIT);
-
-            if (query.length != Constants.TWO) {
-                throw new IllegalArgumentException(Constants.ERROR_FEW_PARAMS);
+            case Constants.CLIENT_REMOVE -> {
+                if (query.length != Constants.THREE)
+                    throw new IllegalArgumentException(Constants.ERROR_FEW_PARAMS);
+                return View.getString(removeClients(query[Constants.TWO]));
             }
-
-            return View.getString(removeClients(query[Constants.ONE]));
-
-        } else {
-            throw new IllegalArgumentException(Constants.ERROR_INVALID_OPTION);
-        }*/
-        return "error";
-    }
-
-    public String clientAddControl(String[] query){
-        //client add "<nombre>" <DNI> <email> <cashId>
-        // Client{identifier='Y8682724P', name='Pepe1', email='pepe3@upm.es', cash=UW1234567}
-        //client add: ok
-        StringBuilder builder = new StringBuilder();
-
-        String name = query[Constants.ONE];
-        String dni = query[Constants.TWO];
-        String email = query[Constants.THREE];
-        String UW = query[Constants.FOUR];
-
-        builder.append(View.getString(addClient(name,dni, email,UW)));
-        builder.append("/n");
-        builder.append(Constants.okStatus("Client","ClientAdd"));
-
-        return builder.toString();
+            default -> throw new IllegalArgumentException(Constants.ERROR_INVALID_OPTION);
+        }
     }
 
     private Client addClient(String name, String dni, String email, String UW) {
@@ -92,11 +51,11 @@ public class ClientController {
     }
 
 
-    public Client removeClients(String id) {
+    private Client removeClients(String id) {
         return clientRepository.removeById(id);
     }
 
-    public Collection<Client> listClients() {
+    private Collection<Client> listClients() {
         return clientRepository.findAll();
     }
 }
